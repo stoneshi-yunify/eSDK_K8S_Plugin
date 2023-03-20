8c896d9f09abd581d30da9da5d757c8cc5535e89
stoneshi-yunify <stoneshi@kubesphere.io>
2023-03-20 14:07:52 +0800

    fix error log
    Signed-off-by: stoneshi-yunify <stoneshi@kubesphere.io>

--- src/csi/driver/node.go
+++ src/csi/driver/node.go
@@ -239,7 +239,7 @@ func (d *Driver) NodeGetVolumeStats(ctx context.Context, req *csi.NodeGetVolumeS
 
 	volumeMetrics, err := utils.GetVolumeMetrics(VolumePath)
 	if err != nil {
-		msg := fmt.Sprintf("get volume metrics failed, reason %v", volumeMetrics)
+		msg := fmt.Sprintf("get volume metrics failed, reason %v", err)
 		log.Errorln(msg)
 		return nil, status.Error(codes.Internal, msg)
 	}
